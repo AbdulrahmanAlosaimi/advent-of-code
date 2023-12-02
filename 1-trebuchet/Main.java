d7240378247313e263c3b09c46e60a73e9ab7301
AbdulrahmanAlosaimi <abdulrahmanosaimi@gmail.com>
2023-12-02 22:35:58 +0300

    init commit

---
+++ 1-trebuchet/Main.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Main {
+  public static void main(String[] args) {
+    try {
+      BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+      int sum = 0;
+
+      // Go through the file, one line at a time
+      for (String line; (line = br.readLine()) != null;) {
+
+        // MAX and MIN values used to make sure we selected first and last digit
+        int leftIndex = Integer.MAX_VALUE, rightIndex = Integer.MIN_VALUE;
+
+        char[] chars = line.toCharArray();
+        for (int i = 0, j = line.length() - 1; i != line.length() && j != -1; i++, j--) {
+
+          if (Character.isDigit(chars[i])) {
+            leftIndex = leftIndex == Integer.MAX_VALUE ? i : Math.min(i, leftIndex);
+          }
+
+          if (Character.isDigit(chars[j])) {
+            rightIndex = rightIndex == Integer.MIN_VALUE ? j : Math.max(j, rightIndex);
+          }
+
+          // If both indexes have moved, no need to check rest of string
+          if (rightIndex != Integer.MIN_VALUE && leftIndex != Integer.MAX_VALUE) {
+            break;
+          }
+        }
+
+        System.out.println(chars);
+        System.out.println("" + chars[leftIndex] + chars[rightIndex] + "\n");
+        sum += Integer.parseInt("" + chars[leftIndex] + chars[rightIndex]);
+      }
+      System.out.println("SUM = " + sum);
+      br.close();
+
+    } catch (Exception e) {
+      // TODO: handle Exception
+      e.printStackTrace();
+    }
+
+  }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of Code
+
+This is my first humble attempt at Advent of Code.
+
+Currently going through 2023's version using Java.
+
+Check out [Advent of Code](https://adventofcode.com/2023)!
