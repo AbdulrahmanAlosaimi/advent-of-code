ef3bbdc9b2152f08a4b4095b9851d75c94ab31ac
AbdulrahmanAlosaimi <abdulrahmanosaimi@gmail.com>
2023-12-05 11:03:03 +0300

    finished part1

--- 2-cube-conundrum/part-1/Main.java
+++ 2-cube-conundrum/part-1/Main.java
@@ -15,43 +15,48 @@ public class Main {
         String[] gameRounds = line.split(":")[1].split(";");
 
         boolean valid = true;
-        for (int i = 0; valid && i < gameRounds.length; i++) {
+        for (int i = 0; i < gameRounds.length; i++) {
 
           String[] selection = gameRounds[i].split(",");
 
           for (int j = 0; valid && j < selection.length; j++) {
 
-            System.out.println(selection[j].trim().split(" ")[1]);
-            System.out.println(Integer.parseInt(selection[j].trim().split(" ")[0]));
-
             if (selection[j].trim().split(" ")[1].equalsIgnoreCase("red")
                 && Integer.parseInt(selection[j].trim().split(" ")[0]) > redMax) {
+
               System.out.println("OVER RED: " + selection[j].trim().split(" ")[0] + " MAX: " + redMax + "\nGAMENUM: "
-                  + gameNum + "\n\n");
-              sum += gameNum;
+                  + gameNum + "\n");
+
               valid = false;
               break;
             }
 
             if (selection[j].trim().split(" ")[1].equalsIgnoreCase("green")
                 && Integer.parseInt(selection[j].trim().split(" ")[0]) > greenMax) {
+
               System.out.println("OVER GREEN: " + selection[j].trim().split(" ")[0] + " MAX: " + greenMax
-                  + "\nGAMENUM: " + gameNum + "\n\n");
-              sum += gameNum;
+                  + "\nGAMENUM: " + gameNum + "\n");
+
               valid = false;
               break;
             }
 
             if (selection[j].trim().split(" ")[1].equalsIgnoreCase("blue")
                 && Integer.parseInt(selection[j].trim().split(" ")[0]) > blueMax) {
+
               System.out.println("OVER BLUE: " + selection[j].trim().split(" ")[0] + " MAX: " + blueMax + "\nGAMENUM: "
-                  + gameNum + "\n\n");
-              sum += gameNum;
+                  + gameNum + "\n");
+
               valid = false;
               break;
             }
           }
-          System.out.println();
+
+        }
+        if (valid) {
+          System.out.println("\nGame number: " + gameNum + "\nSUM BEFORE: " + sum);
+          sum += gameNum;
+          System.out.println("SUM AFTER: " + sum + "\n");
         }
         System.out.println("\n");
       }
